25178af16696d89cc72bbb1e2b27e7ca6c9119a5
bharathi <bharathikarthik2010@gmail.com>
2020-10-21 18:24:28 +1100

    UI
    Design created

--- src/app/premium/monthly-premium/monthly-premium.component.ts
+++ src/app/premium/monthly-premium/monthly-premium.component.ts
@@ -9,6 +9,7 @@ import { FormArray } from '@angular/forms';
 })
 export class MonthlyPremiumComponent implements OnInit {
 
+  
   premiumForm : FormGroup;
   _occupationlist :any;
   _ratingfactorlist : any;
@@ -16,8 +17,8 @@ export class MonthlyPremiumComponent implements OnInit {
 
   ngOnInit(){
     
-    const _occupation = [{ name: "Cleaner", rating: "Light Manual" }, {name: "Doctor", rating: "Professional" }];
-    const _Ratingfactor = [{rating: "Professional" , Factor: 1.0},{rating: "Light Manual" , Factor : 1.50}];
+    const _occupation = [{name: "-- Select Occupation --", rating: "0" },{name: "Cleaner", rating: "Light Manual" }, {name: "Doctor", rating: "Professional" },{name: "Author", rating: "White Collar" },{name: "Farmer", rating: "Heavy Manual" },{name: "Mechanic", rating: "Heavy Manual" },{name: "Florist", rating: "Light Manual" }];
+    const _Ratingfactor = [{rating: "Professional" , Factor: 1.0},{rating: "White Collar" , Factor : 1.25},{rating: "Light Manual" , Factor : 1.50},{rating: "Heavy Manual" , Factor : 1.75}];
     this._occupationlist = _occupation;
     this._ratingfactorlist =_Ratingfactor;
     this.initiateForm();
@@ -26,11 +27,11 @@ export class MonthlyPremiumComponent implements OnInit {
   initiateForm()
   {
     this.premiumForm = this.fb.group({
-      name: ['', Validators.required],
-      age: [0,Validators.required],
+      name: ['', Validators.compose([Validators.required, Validators.pattern('^[A-Za-z]+$'), Validators.maxLength(200)])],
+      age: [,Validators.compose([Validators.required, Validators.pattern('^[0-9]+$'),Validators.maxLength(3)])],
       DOB: ['',Validators.required],
-      occupation: ["Cleaner"],
-      sumInsured: [0]
+      occupation: [0],
+      sumInsured: [,Validators.required]
     });
   }
 
@@ -41,6 +42,7 @@ export class MonthlyPremiumComponent implements OnInit {
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
+    // Formula : Death Premium = (Death Cover amount * Occupation Rating Factor * Age) /1000 * 12
     console.warn(this.premiumForm.value);
   }
 
