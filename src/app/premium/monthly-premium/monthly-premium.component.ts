882c9f5d61b954bf231a290307b89d0588b08283
bharathi <bharathikarthik2010@gmail.com>
2020-10-22 12:25:10 +1100

    Validation for UI fixed

--- src/app/premium/monthly-premium/monthly-premium.component.ts
+++ src/app/premium/monthly-premium/monthly-premium.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder , Validators  ,FormGroup} from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { FormArray } from '@angular/forms';
 
 @Component({
@@ -9,58 +9,72 @@ import { FormArray } from '@angular/forms';
 })
 export class MonthlyPremiumComponent implements OnInit {
 
-  
-  premiumForm : FormGroup;
-  _occupationlist :any;
-  _ratingfactorlist : any;
-  rating_value : any;
-  _calculatedpremium : any;
-  _Infostring :any  ;
+
+  premiumForm: FormGroup;
+  _occupationlist: any;
+  _ratingfactorlist: any;
+  rating_value: any;
+  _calculatedpremium: any;
+  _Infostring: any;
+  occupationinvalid: boolean;
 
   constructor(private fb: FormBuilder) { }
 
-  ngOnInit(){
-    
-    const _occupation = [{name: "-- Select Occupation --", rating: "0" },{name: "Cleaner", rating: "Light Manual" }, {name: "Doctor", rating: "Professional" },{name: "Author", rating: "White Collar" },{name: "Farmer", rating: "Heavy Manual" },{name: "Mechanic", rating: "Heavy Manual" },{name: "Florist", rating: "Light Manual" }];
-    const _Ratingfactor = [{rating: "Professional" , Factor: 1.0},{rating: "White Collar" , Factor : 1.25},{rating: "Light Manual" , Factor : 1.50},{rating: "Heavy Manual" , Factor : 1.75}];
+  ngOnInit() {
+
+    const _occupation = [{ name: "-- Select Occupation --", rating: "0" }, { name: "Cleaner", rating: "Light Manual" }, { name: "Doctor", rating: "Professional" }, { name: "Author", rating: "White Collar" }, { name: "Farmer", rating: "Heavy Manual" }, { name: "Mechanic", rating: "Heavy Manual" }, { name: "Florist", rating: "Light Manual" }];
+    const _Ratingfactor = [{ rating: "Professional", Factor: 1.0 }, { rating: "White Collar", Factor: 1.25 }, { rating: "Light Manual", Factor: 1.50 }, { rating: "Heavy Manual", Factor: 1.75 }];
     this._occupationlist = _occupation;
-    this._ratingfactorlist =_Ratingfactor;
+    this._ratingfactorlist = _Ratingfactor;
     this.initiateForm();
   }
 
-  initiateForm()
-  {
+  initiateForm() {
     this.premiumForm = this.fb.group({
-      name: ['', Validators.compose([Validators.required, Validators.pattern('^[A-Za-z]+$'), Validators.maxLength(200)])],
-      age: [,Validators.compose([Validators.required, Validators.pattern('^[0-9]+$'),Validators.maxLength(3)])],
-      DOB: ['',Validators.required],
+      name: ['', Validators.compose([Validators.required, Validators.pattern('^[a-z]+$'),Validators.maxLength(200)])],
+      age: [, Validators.compose([Validators.required, Validators.pattern('^[0-9]+$')])],
+      DOB: ['', Validators.required],
       occupation: [0],
-      sumInsured: [,Validators.required]
+      sumInsured: [, Validators.compose([Validators.required, Validators.pattern("[0-9.]+$")])]
     });
     this._Infostring = "";
     this._calculatedpremium = "";
+    this.occupationinvalid = false;
+
   }
 
-  onOccupationChange($event)
-  {
+  onOccupationChange($event) {
     // Get Rating factor value on selected occupation
     if ($event) {
       let selected_occupation = $event.target.value;
-       this.rating_value = this._ratingfactorlist.filter(r => (r.rating == selected_occupation))[0].Factor;
+      if (selected_occupation != 0) {
+        this.rating_value = this._ratingfactorlist.filter(r => (r.rating == selected_occupation))[0].Factor;
+        this.occupationinvalid = false;
+      }
+      else {
+        this.occupationinvalid = true;
+      }
     }
   }
 
-
+  clearform() {
+    this.initiateForm();
+  }
 
   onSubmit() {
     // Formula : Death Premium = (Death Cover amount * Occupation Rating Factor * Age) /1000 * 12
     console.warn(this.premiumForm.value);
-    this._calculatedpremium  =  (( this.premiumForm.get('sumInsured').value * this.rating_value * this.premiumForm.get('age').value) /1000 * 12);
+    if (this.premiumForm.get('occupation').value == '0') {
+      this.occupationinvalid = true;
+    }
+    else {
+      this.occupationinvalid = false;
+      this._calculatedpremium = ((this.premiumForm.get('sumInsured').value * this.rating_value * this.premiumForm.get('age').value) / 1000 * 12).toFixed(2);
 
 
-    if(this._calculatedpremium != "")
-    {
-     this._Infostring =  "Based on below inputs,Monthly Premium is  ";
+      if (this._calculatedpremium != "") {
+        this._Infostring = "Based on below input , Calculated Monthly Premium -  ";
+      }
     }
   }
 
